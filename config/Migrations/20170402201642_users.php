9935c50c9da668276bc233709eebd37e73377e47
tim <tim.potter@wanadoo.fr>
2017-04-02 23:57:57 +0200

    Migration Bdd

---
+++ config/Migrations/20170402201642_users.php
@@ -0,0 +1,59 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class Users extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $table = $this->table('users');
+        $table->addColumn('username', 'string', [
+            'default' => null,
+            'limit' => 50,
+            'null' => false,
+        ]);
+        $table->addColumn('password', 'string', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('role', 'string', [
+            'default' => null,
+            'limit' => 20,
+            'null' => false,
+        ]);
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('last_login', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->create();
+    }
+}
